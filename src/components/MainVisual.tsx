6dae4e7ecd14c682b3b2fecc44d1093ecdbc36e9
DESKTOP-DA0NUEO\wwwyo <www.yotta@gmail.com>
2021-10-26 22:16:11 +0900

    fix: メインビジュアルを実装

--- src/components/MainVisual.tsx
+++ src/components/MainVisual.tsx
@@ -1,12 +1,43 @@
 import { NextPage } from "next";
 import Image from "next/image";
-import mv from "images/mv.png";
+import mv from "images/mv_bg.png";
+import yotta from "images/yotta_up.png";
 import styles from "./MainVisual.module.scss";
 
 const MainVisual: NextPage = () => {
   return (
     <div className={styles.root}>
-      <Image src={mv} alt="三波ヨタオフィシャルサイトのメインヴィジュアル" />
+      <div className={styles.loopSlide}>
+        <Image
+          src={mv}
+          width={1920}
+          height={500}
+          alt="三波ヨタオフィシャルサイトのメインヴィジュアル1"
+        />
+        <Image
+          src={mv}
+          width={1920}
+          height={500}
+          alt="三波ヨタオフィシャルサイトのメインヴィジュアル2"
+        />
+      </div>
+      <div className={styles.overlay}></div>
+      <div className={styles.container}>
+        <h1 className={styles.title}>
+          Virtual
+          <br />
+          Web
+          <br />
+          Engineer
+        </h1>
+        <Image
+          src={yotta}
+          width={400}
+          height={450}
+          alt="三波ヨタバストアップ"
+          objectFit="contain"
+        />
+      </div>
     </div>
   );
 };
