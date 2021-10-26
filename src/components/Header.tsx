88cbd6b1ba6d6913eff873ab2f2dd081a6db8a34
DESKTOP-DA0NUEO\wwwyo <www.yotta@gmail.com>
2021-10-26 22:23:46 +0900

    ヘッダーを中央寄せ

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,36 +6,38 @@ import styles from "./Header.module.scss";
 const Header: NextPage = () => {
   return (
     <header className={styles.root}>
-      <Image src="/" alt="ロゴ" width={100} height={40} />
-      <nav>
-        <ul className={styles.menu}>
-          <li>
-            <Link href="#profile">
-              <a>プロフィール</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="#youtube">
-              <a>YouTube</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="#work">
-              <a>仕事</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="#blog">
-              <a>ブログ</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="#contact">
-              <a>お問い合わせ</a>
-            </Link>
-          </li>
-        </ul>
-      </nav>
+      <div className={styles.inner}>
+        <Image src="/" alt="ロゴ" width={100} height={40} />
+        <nav>
+          <ul className={styles.menu}>
+            <li>
+              <Link href="#profile">
+                <a>プロフィール</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="#youtube">
+                <a>YouTube</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="#work">
+                <a>仕事</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="#blog">
+                <a>ブログ</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="#contact">
+                <a>お問い合わせ</a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
