c66e86f2151803cfd284bbf341ef1d6bb66f5bde
DESKTOP-DA0NUEO\wwwyo <www.yotta@gmail.com>
2021-10-28 23:49:24 +0900

    仕事のコンポーネントを共通化

---
+++ src/components/WorkItem.tsx
@@ -0,0 +1,45 @@
+import { FC } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import styles from "./WorkItem.module.scss";
+import { WorkData } from "types/api";
+
+type WorkSection = {
+  item: WorkData;
+};
+const WorkSection: FC<WorkSection> = ({ item }) => {
+  return (
+    <div className={styles.workItem} key={item.id}>
+      <div className={styles.workItemImage}>
+        <Image
+          src={item.image.url}
+          width={320}
+          height={320}
+          alt={item.title}
+          objectFit="cover"
+        />
+      </div>
+      <div className={styles.workItemDescription}>
+        <h3 className={styles.workItemTitle}>{item.title}</h3>
+        <p>{item.description}</p>
+        <div className={styles.workItemLink}>
+          {item.url && (
+            <a
+              href={item.url}
+              target="_blank"
+              className={styles.workItemLink}
+              rel="noreferrer"
+            >
+              作品を見る
+            </a>
+          )}
+          <Link href={`/work/${item.id}`}>
+            <a className={styles.workItemLink}>詳細を見る</a>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WorkSection;
--- src/components/WorkSection.tsx
+++ src/components/WorkSection.tsx
@@ -1,9 +1,9 @@
 import { FC } from "react";
 import Link from "next/link";
-import Image from "next/image";
 import styles from "./WorkSection.module.scss";
 import { WorkData } from "types/api";
 import Button from "@mui/material/Button";
+import WorkItem from "components/WorkItem";
 
 type WorkSection = {
   id: string;
@@ -21,38 +21,7 @@ const WorkSection: FC<WorkSection> = ({ data, ...props }) => {
         </p>
         <div className={styles.work}>
           {data?.map((item) => {
-            return (
-              <div className={styles.workItem} key={item.id}>
-                <div className={styles.workItemImage}>
-                  <Image
-                    src={item.image.url}
-                    width={320}
-                    height={320}
-                    alt="カスタムオーディオパネルの作品画像"
-                    objectFit="cover"
-                  />
-                </div>
-                <div className={styles.workItemDescription}>
-                  <h3 className={styles.workItemTitle}>{item.title}</h3>
-                  <p>{item.description}</p>
-                  <div className={styles.workItemLink}>
-                    {item.url && (
-                      <a
-                        href={item.url}
-                        target="_blank"
-                        className={styles.workItemLink}
-                        rel="noreferrer"
-                      >
-                        作品を見る
-                      </a>
-                    )}
-                    <Link href={`/work/${item.id}`}>
-                      <a className={styles.workItemLink}>詳細を見る</a>
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            );
+            return <WorkItem item={item} key={item.id} />;
           })}
         </div>
         <Link href="/work/page/1" passHref>
