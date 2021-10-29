8ba6492ce2e9b3b72e386c0f803e58d80b1344c4
DESKTOP-DA0NUEO\wwwyo <www.yotta@gmail.com>
2021-10-29 19:40:05 +0900

    ヘッダーのレスポンシブを実装

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,13 @@ class MyDocument extends Document {
   render() {
     return (
       <Html style={{ scrollBehavior: "smooth" }}>
-        <Head />
+        <Head>
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
